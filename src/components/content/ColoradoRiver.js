7f2609af6420f0306a6f5837dc28ae63ccb63597
artimusmaximus <arthurmodymaniv@gmail.com>
2022-06-07 11:59:26 -0700

    updating coloradoriver component

--- src/components/content/ColoradoRiver.js
+++ src/components/content/ColoradoRiver.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import Sidebar from '../Sidebar';
+import VirtualTour from '../VirtualTour';
+import img0 from "./images/coloradoriverimages/1.webp"
+import img1 from "./images/coloradoriverimages/10.webp"
+import img2 from "./images/coloradoriverimages/11.webp"
+import img3 from "./images/coloradoriverimages/12.webp"
+import img4 from "./images/coloradoriverimages/13.webp"
+import img5 from "./images/coloradoriverimages/14.webp"
+import img6 from "./images/coloradoriverimages/15.webp"
+import img7 from "./images/coloradoriverimages/16.webp"
+import img8 from "./images/coloradoriverimages/2.webp"
+import img9 from "./images/coloradoriverimages/3.webp"
+import img10 from "./images/coloradoriverimages/4.webp"
+import img11 from "./images/coloradoriverimages/5.webp"
+import img12 from "./images/coloradoriverimages/6.webp"
+import img13 from "./images/coloradoriverimages/7.webp"
+import img14 from "./images/coloradoriverimages/8.webp"
+import img15 from "./images/coloradoriverimages/9.webp"
 
 
 function ColoradoRiver(props) {
+
+    const arr = [
+        img0,  img1,  img2,
+        img3,  img4,  img5,
+        img6,  img7,  img8,
+        img9,  img10, img11,
+        img12, img13, img14,
+        img15
+    ];
+
     return (
         <>
         <div className='row'>
@@ -15,7 +42,7 @@ function ColoradoRiver(props) {
                 
             </h1>
             <h3>
-            1326 E Dike Rd. Mohave Valley&nbsp;, Arizona
+                1326 E Dike Rd. Mohave Valley&nbsp;, Arizona
             </h3>
             <h5>
                 Colorado River For Your California Vacation
